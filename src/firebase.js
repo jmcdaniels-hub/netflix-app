144e25fda17687fac8f61f86c954b86dd04c5f20
jmcdaniels-hub <jademcdaniels81@gmail.com>
2021-09-04 11:11:59 -0400

    nav bar has scroll function

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCVpn1XNajSBJLzQTYZJGthSwFNX_kdWmw",
+    authDomain: "netflix-clone-28a90.firebaseapp.com",
+    projectId: "netflix-clone-28a90",
+    storageBucket: "netflix-clone-28a90.appspot.com",
+    messagingSenderId: "1028458379066",
+    appId: "1:1028458379066:web:efb1f6f930040fbcf5c203"
+  };
