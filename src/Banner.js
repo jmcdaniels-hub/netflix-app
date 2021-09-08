c1f484fa53b1dd028add3d290e10a982176cbf63
jmcdaniels-hub <jademcdaniels81@gmail.com>
2021-09-08 11:57:53 -0400

    completed banner with truncated descriptions

--- src/Banner.js
+++ src/Banner.js
@@ -2,10 +2,34 @@ import React from 'react'
 import "./Banner.css"
 
 function Banner() {
+
+    function truncate(string, n){
+        return string?.length > n ? string.substr(0, n-1) + '...' : string;
+    }
+
+
     return (
-        <div className="banner">
-            
-        </div>
+        <header className="banner" style={{
+            backgroundSize: "cover",
+            // backgroundImage: `url(https://i.imgur.com/e1hLQ2m.png)`,
+            backgroundImage: `url(https://www.solidbackgrounds.com/images/1920x1080/1920x1080-black-solid-color-background.jpg)`,
+            backgroundPosition: "center center"
+        }}>
+            <div className="banner_contents">
+                <h1 className="banner_title">Movie Name</h1>
+                <div className="banner_buttons">
+                     <button className="banner_button">Play</button>
+                     <button className="banner_button"> My List</button>
+                </div>
+                <h1 className="banner_discription">
+                    {truncate(`
+                    This is a test description sdlfkslkvn;sknv;sklnvksndvksvlksldkvlskvksnvlksnvlknsvknslvknslkvnslknvsnvklsvkns;lkvn;skjnv;js;jdf.v svj;svsknvknv'wknf/wkv;snvsknvksjvb;bvdjv.jbv;bv;ajd .jbd;vk
+                    `,150)}                   
+                 </h1>
+            </div>
+
+            <div className="banner--fadeButton"/>
+        </header>
     )
 }
 
